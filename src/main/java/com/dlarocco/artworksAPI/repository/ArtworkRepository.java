a953b6f5f4793d40f81551f0bb1e94aa6c2c0922
David LaRocco <dlarocco77@gmail.com>
2023-09-02 17:39:56 -0600

    add repository annotation to ArtworkRepository

--- src/main/java/com/dlarocco/artworksAPI/repository/ArtworkRepository.java
+++ src/main/java/com/dlarocco/artworksAPI/repository/ArtworkRepository.java
@@ -2,7 +2,8 @@ package com.dlarocco.artworksAPI.repository;
 
 import com.dlarocco.artworksAPI.repository.entity.Artwork;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-
+@Repository
 public interface ArtworkRepository extends JpaRepository<Artwork, Integer> {
 }
